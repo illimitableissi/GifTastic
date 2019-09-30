d09ddc3352ffa90a804d9a2468ff1424ffc7c87e
SOVEREIGN <isimemeni@gmail.com>
2019-09-30 00:04:44 -0400

    cleaned up code

--- giftastic.js
+++ giftastic.js
@@ -1,62 +1,58 @@
 //Array for One-Piece topic
 var topics = ["luffy", "roronoa zoro", "usopp", "nami", "shanks", "portgas d. ace", "monkey d. garp", "sanji", "akainu", "kuzan", "dracule mihawk", "enel", "sabo"]
 
+//Function to create buttons on HTML
 function createButtons () {
-
+    //clears buttons on HTML
     $("#buttons").empty();
 
-//For loop to produce buttons for strings
+    //For loop to produce buttons from array
     for (var i = 0; i < topics.length; i++) {
-    
         var topicButtons = $("<button>");
         topicButtons.addClass("topic-button");
         topicButtons.attr("data-topic", topics[i])
         topicButtons.text(topics[i]);
         $('#buttons').append(topicButtons)
-    }       
+    }    
+}   
 
-//Defines function on click
+//Function produces gifs and rating on click
+function clickAction() {
+    
     $(".topic-button").on("click", function(event) {
-
         event.preventDefault();
+        var characterName =$(this).attr("data-topic")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kVuH86l1irJhDIHRaV7trtaOBiICrbNu&limit=10&q=" + characterName
 
-    var characterName =$(this).attr("data-topic")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kVuH86l1irJhDIHRaV7trtaOBiICrbNu&limit=10&q=" + characterName
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    console.log(response.data);
-
-var results = response.data;
-
-for (var g = 0; g < results.length; g++) {
-
-    console.log(results[g].images.fixed_height.url)
-
-var gifs = $("<img>")
-var rating = results[g].rating
-var p = $("<p>").text("Rating: " + rating);
-
-gifs.attr("src", results[g].images.fixed_height.url);
-
-
-$("#gifs").append(gifs)
-$("#gifs").append(p)
-
+        var results = response.data;
+        //Loops through data array
+        for (var g = 0; g < results.length; g++) {
+            var gifs = $("<img>");
+            gifs.attr("src", results[g].images.fixed_height.url);
+            var rating = results[g].rating;
+            var p = $("<p>").text("Rating: " + rating);          
+            $("#gifs").append(gifs)
+            $("#gifs").append(p)
+            }  
+        });
+    });
 }
 
-});
-});
-}
+
 //Creates new button from user input
     $("#find-character").on("click", function(event) {
         event.preventDefault();
         var input = $("#user-input").val().trim();
         topics.push(input);
         createButtons()
+        clickAction()
         })
-
-createButtons()
+        
+//invokes functions
+createButtons();
+clickAction();
